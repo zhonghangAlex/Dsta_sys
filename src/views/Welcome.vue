a63dc0c965605e03cffce08346f182b792363970
zhonghangAlex <715608270@qq.com>
2019-03-03 12:30:09 +0800

    2019.3.3完善登录注册页面相关内容

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="lowin" v-if="showView">
       <div class="lowin-brand">
-        <img :src="img_src" alt="logo">
+        <img :src="Img_src" alt="logo">
       </div>
       <div class="lowin-wrapper">
         <div class="lowin-box lowin-login">
@@ -10,19 +10,19 @@
             <div>
               <p>登录开始数字签名之旅</p>
               <div class="lowin-group">
-                <label>邮箱 <a href="#" class="login-back-link">直接登录?</a></label>
-                <input type="email" autocomplete="email" name="email" class="lowin-input email_ipt">
+                <label>账号 <a href="#" class="login-back-link" ref="change_login" @click="viewChangeStatus(1)">直接登录?</a></label>
+                <input name="no" class="lowin-input email_ipt" ref="login_no" v-model="userID">
               </div>
               <div class="lowin-group password-group">
-                <label>密码 <a href="#" class="forgot-link">忘记密码了吗?</a></label>
-                <input type="password" name="password" autocomplete="current-password" class="lowin-input password_ipt">
+                <label>密码 <a href="#" class="forgot-link" ref="change_forget" @click="viewChangeStatus(3)">忘记密码了吗?</a></label>
+                <input type="password" name="password" autocomplete="current-password" class="lowin-input password_ipt" ref="login_psw" v-model="userPassword">
               </div>
-              <button class="lowin-btn login-btn" @click="userLogin">
+              <button class="lowin-btn login-btn" @click="userLoginAndFind">
                 登&nbsp;&nbsp;&nbsp;录
               </button>
 
               <div class="text-foot">
-                还没有一个账号吗? <a href="" class="register-link">注册</a>
+                还没有一个账号吗? <a href="" class="register-link" ref="change_register" @click="viewChangeStatus(2)">注册</a>
               </div>
             </div>
           </div>
@@ -34,22 +34,22 @@
               <p>让我们来创建你的账号</p>
               <div class="lowin-group">
                 <label>姓名</label>
-                <input type="text" name="name" autocomplete="name" class="lowin-input">
+                <input type="text" name="name" autocomplete="name" class="lowin-input" v-model="userRegName">
               </div>
               <div class="lowin-group">
-                <label>邮箱</label>
-                <input type="email" autocomplete="email" name="email" class="lowin-input">
+                <label>账号</label>
+                <input name="no" class="lowin-input" v-model="userRegID">
               </div>
               <div class="lowin-group">
                 <label>密码</label>
-                <input type="password" name="password" autocomplete="current-password" class="lowin-input">
+                <input type="password" name="password" autocomplete="current-password" class="lowin-input" v-model="userRegPassword">
               </div>
-              <button class="lowin-btn">
+              <button class="lowin-btn" @click="userRegister">
                 点击注册
               </button>
 
               <div class="text-foot">
-                已经拥有一个账号? <a href="" class="login-link">登录</a>
+                已经拥有一个账号? <a href="" class="login-link" ref="change_login" @click="viewChangeStatus(1)">登录</a>
               </div>
             </div>
           </div>
@@ -64,16 +64,25 @@
 </template>
 
 <script>
+  import {Login, Register, FindPassword} from '../api/api'
+
   export default {
     data() {
       return {
         showView: false,
-        img_src: require("../assets/images/logo.svg"),
-        view_status: 1
+        Img_src: require("../assets/images/logo.svg"),
+        // 登录视图为1，注册视图为2，密码找回视图为3
+        viewStatus: 1,
+        // 登录及找回密码
+        userID: '',
+        userPassword: '',
+        // 注册
+        userRegID: '',
+        userRegName: '',
+        userRegPassword: '',
+        //返回获取
+        userPower: ''
       }
-    },
-    updated() {
-
     },
     mounted() {
       this.showViewChange()
@@ -227,19 +236,102 @@
           });
         })
       },
+      viewChangeStatus: function(status_code) {
+        let _this = this
+        if (status_code == '1') {
+          _this.viewStatus = 1
+        }else if(status_code == '2'){
+          _this.viewStatus = 2
+        }else if (status_code == '3') {
+          _this.viewStatus = 3
+        }
+      },
+      userLoginAndFind: function () {
+        let _this = this
+        if (_this.viewStatus == '1'){
+          _this.userLogin()
+        }else {
+          _this.userFind()
+        }
+      },
       userLogin: function () {
-        this.$http.post('url',{
-          email: "123",
-          password: "woaini123"
-        }).then((response)=>{
-          let res = response
-          if (res.data.status == '0'){
-            this.$router.push("/ordindex")
+        let _this = this
+        let params = {
+          userid: _this.userID,
+          userpassword: _this.userPassword
+        }
+        Login(params).then((response)=>{
+          let res = response.data
+          if (res.status == '0' && res.result.power == '2'){
+            _this.$message({
+              message: res.message,
+              type: 'success',
+              duration:2000,
+              showClose:true
+            });
+            _this.$router.push("/ordindex")
+          }else if(res.status == '0' && res.result.power == '1') {
+            _this.$message({
+              message: res.message,
+              type: 'success',
+              duration:2000,
+              showClose:true
+            });
+            _this.$router.push("/superindex")
           }else {
-
+            _this.$message({
+              message: res.message,
+              type: 'error',
+              duration: 2000,
+              showClose: true
+            });
           }
         }).catch((err)=>{
           console.log(err)
+          _this.$message({
+            message: '数据请求失败，请检查网络',
+            type: 'error',
+            duration:2000,
+            showClose:true
+          });
+        })
+      },
+      userFind: function () {
+        return
+      },
+      userRegister: function () {
+        let _this = this
+        let params = {
+          userid: _this.userRegID,
+          username: _this.userRegName,
+          userpassword: _this.userRegPassword
+        }
+        Register(params).then((response)=>{
+          let res = response.data
+          if (res.status == '0'){
+            _this.$message({
+              message: res.message,
+              type: 'success',
+              duration:2000,
+              showClose:true
+            });
+            _this.$router.push("/ordindex")
+          }else {
+            _this.$message({
+              message: res.message,
+              type: 'error',
+              duration: 2000,
+              showClose: true
+            });
+          }
+        }).catch((err)=>{
+          console.log(err)
+          _this.$message({
+            message: '数据请求失败，请检查网络',
+            type: 'error',
+            duration:2000,
+            showClose:true
+          });
         })
       }
     }
